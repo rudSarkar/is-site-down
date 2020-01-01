1aeef30ecd1adfde0dcd6d12d9a6e124d4186312
Rudra Sarkar <rudrasarkar815@gmail.com>
2020-01-02 01:54:34 +0600

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -9,9 +9,4 @@ from IsSiteDown import app
 
 if __name__ == '__main__':
     app.secret_key = "super secret key"
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    app.run()
