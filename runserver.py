98cc517a82977b95af70aee4f9aa3247069a3d37
rudSarkar <rudrasarkar815@gmail.com>
2020-01-02 01:43:15 +0600

    remove debugger

--- runserver.py
+++ runserver.py
@@ -9,7 +9,6 @@ from IsSiteDown import app
 
 if __name__ == '__main__':
     app.secret_key = "super secret key"
-    app.debug = True
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
